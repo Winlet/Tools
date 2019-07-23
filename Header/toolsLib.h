59330a80c30580452e2abf51e2ca38d3e78d8277
uinpay <uinpay@uinpaydeMac-mini.local>
2019-07-23 09:41:05 +0800

    add lib

---
+++ Header/toolsLib.h
@@ -0,0 +1,16 @@
+//
+//  toolsLib.h
+//  toolsLib
+//
+//  Created by uinpay on 2019/7/19.
+//  Copyright © 2019 刘起伏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface toolsLib : NSObject
+
+//获取日期 yyyy-M-d
+@property (nonatomic,strong) NSMutableArray *yearMDArray;
+
+@end
